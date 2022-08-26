7e8982ef7a28f076aaf7ff88c85872371ca3425f
DerBot7214 <lengwenath1@gmail.com>
2022-08-26 07:08:52 +0200

    router

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
 
 function App() {
-  return <></>
+  return <>
+    <Routes>
+      <Route path='/' element={<>Hi</>} />
+      <Route path='about' element={<>Rboard Homepage</>} />
+    </Routes>
+  </>
 }
 
 export default App
