771621ea373b066a36762270ee6811b1a5df0f07
DerTyp7214 <lengwenath1@gmail.com>
2022-08-28 10:01:58 +0200

    cname

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,16 @@ import { Route, Routes } from 'react-router-dom'
 import Apps from './pages/apps/Apps'
 
 const App = () => {
-  return <>
-    <Routes>
-      <Route path='*' element={<>404</>} />
-      <Route path='/' element={<>Hi</>} />
-      <Route path='apps' element={<Apps />}>
-        <Route path=':appId' element={<Box sx={{ background: 'green' }}></Box>} />
-      </Route>
-      <Route path='about' element={<>Rboard Homepage</>} />
-    </Routes>
-  </>
+    return <>
+        <Routes>
+            <Route path='*' element={<>404</>} />
+            <Route path='/' element={<>Hi</>} />
+            <Route path='apps' element={<Apps />}>
+                <Route path=':appId' element={<Box sx={{ background: 'green' }}></Box>} />
+            </Route>
+            <Route path='about' element={<>Rboard Homepage</>} />
+        </Routes>
+    </>
 }
 
 export default App
